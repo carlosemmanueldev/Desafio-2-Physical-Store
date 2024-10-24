0dd76b01d5e10636e85d248bbe841da229bebd35
Carlos <carlos.emmanuel.236@gmail.com>
2024-10-24 19:00:44 -0300

    Adicionando conexão com o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express, { Request, Response } from 'express';
+import storeRouter from "./routes/storeRoutes";
 
 const app = express();
-const port = 3000;
 
-app.listen(port, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World');
-});
+app.use('/', storeRouter);
+
+export default app;
