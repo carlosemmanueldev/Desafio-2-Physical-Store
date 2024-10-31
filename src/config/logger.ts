221677e8880bcf6c8d005c67f0b67f23a00df35d
Carlos <carlos.emmanuel.236@gmail.com>
2024-10-31 04:44:16 -0300

    Adicionando logger e suas configurações

---
+++ src/config/logger.ts
@@ -0,0 +1,37 @@
+import { createLogger, format, transports } from 'winston';
+import 'winston-mongodb';
+import dotenv from "dotenv";
+
+dotenv.config({path: '.env'});
+const { combine, timestamp, printf, colorize, errors, json } = format;
+
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+    return `${timestamp} [${level}]: ${stack || message}`;
+});
+
+const logger = createLogger({
+    level: 'info',
+    format: combine(
+        timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+        errors({ stack: true }),
+        json()
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new transports.Console({
+            format: combine(colorize(), logFormat)
+        }),
+    ],
+});
+
+if (process.env.NODE_ENV === 'production') {
+    logger.add( new transports.MongoDB({
+        level: 'error',
+        db: process.env.DATABASE!.replace('<DATABASE_PASSWORD>', process.env.DATABASE_PASSWORD!),
+        collection: 'server_logs',
+        tryReconnect: true,
+        format: combine(timestamp(), json()),
+    }));
+}
+
+export default logger;
