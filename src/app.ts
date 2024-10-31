921a6f73f689e14b12aa3805965d22ec3a9dfb3f
Carlos <carlos.emmanuel.236@gmail.com>
2024-10-31 19:34:03 -0300

    Corrigindo textos do app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import logger from "./config/logger";
 const app = express();
 
 app.use((req, res, next) => {
-   logger.info(`Requisição recebida: ${req.method} ${req.url}`);
+   logger.info(`Request received: ${req.method} ${req.url}`);
    next();
 });
 
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use('/api/v1/stores', storeRouter);
 
 app.all('*', (req, res, next) => {
-   const message = `Não foi possível encontrar ${req.originalUrl} neste servidor!`;
+   const message = `Could not find ${req.originalUrl} on this server!`;
    logger.warn(message);
    next(new AppError(message, 404));
 });
