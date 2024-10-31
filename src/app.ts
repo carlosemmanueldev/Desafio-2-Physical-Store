3a565f68651bb4a261fbe48115de270b7dcacc41
Carlos <carlos.emmanuel.236@gmail.com>
2024-10-31 04:45:41 -0300

    Substituindo logs nativos por logs do winston

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,23 @@ import express from 'express';
 import AppError from "./utils/appError";
 import storeRouter from "./routes/storeRoutes";
 import {globalErrorHandler} from "./controllers/errorController";
+import logger from "./config/logger";
 
 const app = express();
+
+app.use((req, res, next) => {
+   logger.info(`Requisição recebida: ${req.method} ${req.url}`);
+   next();
+});
 
 app.use(express.json());
 
 app.use('/api/v1/stores', storeRouter);
 
 app.all('*', (req, res, next) => {
-   next(new AppError(`Não foi possível encontrar ${req.originalUrl} neste servidor!`, 404));
+   const message = `Não foi possível encontrar ${req.originalUrl} neste servidor!`;
+   logger.warn(message);
+   next(new AppError(message, 404));
 });
 
 app.use(globalErrorHandler);
